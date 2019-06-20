88f341387d79e312b57f22f4e954172c7d96492c
Veronica Nutting <veronicanutting@college.harvard.edu>
2019-06-20 10:06:16 +0200

    just hardcoded minesAround values

--- Minesweeper/app/src/main/java/us/harvard/minesweeper/model/MinesweeperModel.kt
+++ Minesweeper/app/src/main/java/us/harvard/minesweeper/model/MinesweeperModel.kt
@@ -10,16 +10,32 @@ object MinesweeperModel {
     public fun resetModel() {
         for (i in 0..4) {
             for (j in 0..4) {
-                fieldMatrix[i][j].wasClicked = false
                 fieldMatrix[i][j].isBomb = false
-                fieldMatrix[i][j].isFlagged = false
                 fieldMatrix[i][j].minesAround = 0
+                fieldMatrix[i][j].isFlagged = false
+                fieldMatrix[i][j].wasClicked = false
             }
         }
 
-        // hardcoding Bombs
+        // hardcoding bombs
         fieldMatrix[0][0].isBomb = true
         fieldMatrix[1][1].isBomb = true
+        fieldMatrix[2][2].isBomb = true
+
+        // hardcoding minesAround values
+        // this will be a test of whether fist index is "x" or "y"
+        fieldMatrix[0][1].minesAround = 2
+        fieldMatrix[0][2].minesAround = 1
+        fieldMatrix[1][0].minesAround = 2
+        fieldMatrix[1][2].minesAround = 2
+        fieldMatrix[1][3].minesAround = 1
+        fieldMatrix[2][0].minesAround = 1
+        fieldMatrix[2][1].minesAround = 2
+        fieldMatrix[2][3].minesAround = 1
+        fieldMatrix[2][3].minesAround = 1
+        fieldMatrix[3][1].minesAround = 1
+        fieldMatrix[3][2].minesAround = 1
+        fieldMatrix[3][3].minesAround = 1
 
     }
 
