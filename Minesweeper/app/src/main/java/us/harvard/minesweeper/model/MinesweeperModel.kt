22703a54caa6fce981ef1940d0e06f97efb33904
Veronica Nutting <veronicanutting@college.harvard.edu>
2019-06-20 22:49:33 -0300

    bombs are randomly generated; now tackling dynamic minesAround counting

--- Minesweeper/app/src/main/java/us/harvard/minesweeper/model/MinesweeperModel.kt
+++ Minesweeper/app/src/main/java/us/harvard/minesweeper/model/MinesweeperModel.kt
@@ -1,11 +1,15 @@
 package us.harvard.minesweeper.model
 
+import android.util.Log
 import java.util.*
+import kotlin.random.Random
 
 object MinesweeperModel {
 
-    var fieldMatrix = Array<Array<Field>>(5) { Array<Field>(5)
-    {Field(false, 0, false,false)} }
+    var fieldMatrix = Array<Array<Field>>(5) {
+        Array<Field>(5)
+        { Field(false, 0, false, false) }
+    }
 
     public fun resetModel() {
         for (i in 0..4) {
@@ -17,13 +21,51 @@ object MinesweeperModel {
             }
         }
 
+
+        // RANDOMLY GENERATES INDEXES FOR SOME # OF BOMBS
+        val NUM_BOMBS : Int = 3
+
+        var x : Int = 0
+        var y : Int = 0
+        var bombs_placed : Int = 0
+
+        while (bombs_placed < NUM_BOMBS - 1) {
+            Log.d("RAND", "GENERATING RANDOM NUMS FOR BOMB PLACEMENT")
+            x = Random.nextInt(0, 5)
+            y = Random.nextInt(0, 5)
+            Log.d("RAND", "Bomb #$bombs_placed is at ($x, $y)")
+
+            if (!fieldMatrix[x][y].isBomb) {
+                fieldMatrix[x][y].isBomb = true
+                bombs_placed++
+
+                // now will do mines around stuff
+            }
+
+        }
+
+        /*
         // hardcoding bombs
         fieldMatrix[0][0].isBomb = true
         fieldMatrix[1][1].isBomb = true
         fieldMatrix[2][2].isBomb = true
+*/
+
+        // CALCULATES MINESAROUND VALUES FOR WHOLE BOARD
+        // this could go in the for loop that generates bombs
+
+        /*
+
+        // would be something like
+
+
+         */
+
+
+
+
 
         // hardcoding minesAround values
-        // this will be a test of whether fist index is "x" or "y"
         fieldMatrix[0][1].minesAround = 2
         fieldMatrix[0][2].minesAround = 1
         fieldMatrix[1][0].minesAround = 2
@@ -38,6 +80,14 @@ object MinesweeperModel {
         fieldMatrix[3][3].minesAround = 1
 
     }
+
+    /*
+    //private fun generateRandom(): Random = Random(5)
+
+    private fun generateRandom(a : Int) : Int {
+        val rand = java.util.Random(a.toLong())
+        return rand.nextInt(5) // number between 0 and 99 inclusive
+    }*/
 
 }
 
