5103a5931625c670a0496a96ceb476cd0a80cc9e
Veronica Nutting <veronicanutting@college.harvard.edu>
2019-07-07 16:17:29 +0200

    checkbox and splash still a little buggy

--- ShopProject/app/src/main/java/us/harvard/shopproject/adapter/ShopItemAdapter.kt
+++ ShopProject/app/src/main/java/us/harvard/shopproject/adapter/ShopItemAdapter.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.shop_item_row.view.*
 import us.harvard.shopproject.R
@@ -22,16 +23,6 @@ class ShopItemAdapter : RecyclerView.Adapter<ShopItemAdapter.ViewHolder>{
         shopItems.addAll(shopList)
     }
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val cbShopItemPurchased = itemView.cbShopItemPurchased
-        val tvShopItemName = itemView.tvShopItemName
-        val tvShopItemDescription = itemView.tvShopItemDescription
-        val tvShopItemEstimatedPrice = itemView.tvShopItemEstimatedPrice
-        val ivShopItemCategory = itemView.ivShopItemCategory
-        val btnEditShopItem = itemView.btnEditShopItem
-        val btnDeleteShopItem = itemView.btnDeleteShopItem
-    }
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val shopItemView = LayoutInflater.from(context).inflate(
             R.layout.shop_item_row, parent, false)
@@ -42,35 +33,14 @@ class ShopItemAdapter : RecyclerView.Adapter<ShopItemAdapter.ViewHolder>{
         return shopItems.size
     }
 
-    var category : String = "food"
-
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         var shopItem = shopItems.get(holder.adapterPosition)
-
-        Log.d("CHECK", "Here is shopItemPurchased: ${shopItem.shopItemPurchased}")
-        Log.d("CHECK", "Here is cbShopItemPurchased: ${holder.cbShopItemPurchased.isChecked}")
 
         holder.cbShopItemPurchased.isChecked = shopItem.shopItemPurchased
         holder.tvShopItemName.text = shopItem.shopItemName
         holder.tvShopItemDescription.text = shopItem.shopItemDescription
         holder.tvShopItemEstimatedPrice.text = shopItem.shopItemEstimatedPrice
-        category = shopItem.shopItemCategory
-
-        Log.d("CHECKEDDDDD", "Here is cbShopItemPurchased: ${holder.cbShopItemPurchased.isChecked}")
-
-        when (category) {
-            "fruits" -> holder.ivShopItemCategory.setImageResource(R.drawable.fruit)
-            "vegetables" -> holder.ivShopItemCategory.setImageResource(R.drawable.veggie)
-            "meat and eggs" -> holder.ivShopItemCategory.setImageResource(R.drawable.meat)
-            "dairy" -> holder.ivShopItemCategory.setImageResource(R.drawable.cheese)
-            "fish" -> holder.ivShopItemCategory.setImageResource(R.drawable.fish)
-            "grains" -> holder.ivShopItemCategory.setImageResource(R.drawable.grain)
-            "beverages" -> holder.ivShopItemCategory.setImageResource(R.drawable.beverages)
-            "alcohol" -> holder.ivShopItemCategory.setImageResource(R.drawable.alcohol)
-            "sweets" -> holder.ivShopItemCategory.setImageResource(R.drawable.sweets)
-            "cleaning supplies" -> holder.ivShopItemCategory.setImageResource(R.drawable.cleaning)
-            else -> holder.ivShopItemCategory.setImageResource(R.drawable.other)
-        }
+        setHolderCategoryImage(shopItem.shopItemCategory, holder)
 
         holder.btnDeleteShopItem.setOnClickListener{
             removeShopItem(holder.adapterPosition)
@@ -83,8 +53,7 @@ class ShopItemAdapter : RecyclerView.Adapter<ShopItemAdapter.ViewHolder>{
             shopItem.shopItemPurchased = holder.cbShopItemPurchased.isChecked
 
             Thread{
-                AppDatabase.getInstance(context).shopItemDao()
-                    .updateShopItem(shopItem)
+                AppDatabase.getInstance(context).shopItemDao().updateShopItem(shopItem)
             }.start()
         }
     }
@@ -114,5 +83,29 @@ class ShopItemAdapter : RecyclerView.Adapter<ShopItemAdapter.ViewHolder>{
         notifyItemChanged(index)
     }
 
-    // COULD ADD ON ITEM MOVED
+    fun setHolderCategoryImage(category : String, holder: ViewHolder) {
+        when (category) {
+            "fruits" -> holder.ivShopItemCategory.setImageResource(R.drawable.fruit)
+            "vegetables" -> holder.ivShopItemCategory.setImageResource(R.drawable.veggie)
+            "meat and eggs" -> holder.ivShopItemCategory.setImageResource(R.drawable.meat)
+            "dairy" -> holder.ivShopItemCategory.setImageResource(R.drawable.cheese)
+            "fish" -> holder.ivShopItemCategory.setImageResource(R.drawable.fish)
+            "grains" -> holder.ivShopItemCategory.setImageResource(R.drawable.grain)
+            "beverages" -> holder.ivShopItemCategory.setImageResource(R.drawable.beverages)
+            "alcohol" -> holder.ivShopItemCategory.setImageResource(R.drawable.alcohol)
+            "sweets" -> holder.ivShopItemCategory.setImageResource(R.drawable.sweets)
+            "cleaning supplies" -> holder.ivShopItemCategory.setImageResource(R.drawable.cleaning)
+            else -> holder.ivShopItemCategory.setImageResource(R.drawable.other)
+        }
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val cbShopItemPurchased = itemView.cbShopItemPurchased
+        val tvShopItemName = itemView.tvShopItemName
+        val tvShopItemDescription = itemView.tvShopItemDescription
+        val tvShopItemEstimatedPrice = itemView.tvShopItemEstimatedPrice
+        val ivShopItemCategory = itemView.ivShopItemCategory
+        val btnEditShopItem = itemView.btnEditShopItem
+        val btnDeleteShopItem = itemView.btnDeleteShopItem
+    }
 }
